823d95d20c82c55bd7167e0a20c803b591d8191c
Danilo T. Recchia <danilo.recchia@vortus.solutions>
2024-07-15 23:14:45 +0000

    Reorganizando options e renomeando botao seletor de filtros.

--- src/js/filterbar.js
+++ src/js/filterbar.js
@@ -1,52 +1,81 @@
+class ClassNames {
+    static ROOT = 'hpv-filter-bar';
+    static FLOATING_CONTENT = 'floating-content';
+    static IS_READONLY = 'is-readonly';
+
+    static PICKER_BTN = 'picker-btn';
+    static PICKER_FLOATING_CONTENT = 'picker-filter-content';
+
+    static SELECTOR_BTN = 'selector-btn';
+    static SELECTOR_BTN_DISABLED = 'selector-btn-disabled';
+    static SELECTOR_BTN_TEXT = 'selector-btn-text';
+    static SELECTOR_BTN_ARROW_CONTAINER = 'selector-btn-arrow-container';
+    static SELECTOR_BTN_ARROW = 'selector-btn-arrow';
+    static SELECTOR_CONTENT_HEADER = 'filter-content-header';
+    static SELECTOR_CONTENT_BODY = 'filter-content-body';
+    static SELECTOR_CONTENT_ITEM = 'filter-content-item';
+}
+
 class HpvFilterBar {
-    constructor(containerId, opts) {
-        const defaultOpts = {
-            masterBtnTitle: 'Select filter',
+    constructor(containerId, opts = {}) {
+        this.options = {
+            pickerBtnTitle: 'Select filter',
             attachDropdown: (source, target) => {},
             onToggleDropdown: (source, target) => {},
+            ...opts
         };
 
-        this.options = Object.assign({}, defaultOpts, opts);
         this.container = document.getElementById(containerId);
         this.filters = new Map();
-
-        this.rootClass = 'hpv-filter-bar';
-        this.floatingContentClass = 'floating-content';        
-        this.masterBtnClass = 'master-btn';
-        this.masterBtnFloatingContentClass = 'floating-content-master-btn';
-
-        this.masterBtn = null;
+        this.pickerBtn = null;
         this.init();
     }
 
     init() {
-        this.setupMainContainer();
-        this.setupmasterBtn();
-    }
-
-    setupmasterBtn() {
-        this.masterBtn = new MasterBtn(this);
-    }
-
-    setupMainContainer() {
         // add class .hpv-filter-bar to container
-        this.container.classList.add(this.rootClass);
+        this.container.classList.add(ClassNames.ROOT);
+        // create pickerBtn
+        this.pickerBtn = new PickerBtn(this);
+        // bubble events
+        this.setupEventListeners();
+    }
+
+    setupEventListeners() {
+        // listen for filter btn click ( user wants to display filter options )
+        this.container.addEventListener('filterBtnItemSelection', (e) => {
+            e.stopImmediatePropagation();
+            
+            const filter = e.detail.filter;
+            console.log('Opening dropdown for filter ' + filter.getId());
+        });
+
+        // listen for filter item click ( user is adding a filter to filter bar )
+        this.container.addEventListener('pickerBtnItemSelection', (e) => {
+            e.stopImmediatePropagation();
+
+            const filter = e.detail.filter;
+            // add to filter bar
+            filter.addToScreen();
+            // hide dropdown
+            this.pickerBtn.toggleFloatingContentVisibility();
+        });
     }
 
     removeFilter(filterId) {
         const filter = this.filters.get(filterId);
-        if ( !filter ) return;
 
-        filter.removeFromScreen();
-        this.filters.delete(filterId);
+        if ( filter ) {
+            filter.removeFromScreen();
+            this.filters.delete(filterId);
+        }
     }
 
     addFilter(filter) {
-        filter.setParent(this);
+        filter.setBar(this);
         this.filters.set(filter.getId(), filter);
 
         const menuEntry = filter.createMenuEntry();
-        this.masterBtn.addMenuEntry(menuEntry);
+        this.pickerBtn.addMenuEntry(menuEntry);
 
         if ( filter.options.immediateDisplay ) {
             filter.addToScreen();
@@ -54,11 +83,11 @@ class HpvFilterBar {
     }
 
     getMergedRules() {
-        return {};
+        return Array.from(this.filters.values()).reduce((acc, filter) => ({...acc, ...filter.getRules()}), {});
     }
 }
 
-class MasterBtn {
+class FilterBtn {
     constructor(parent) {
         this.parent = parent;
         this.options = parent.options;
@@ -67,62 +96,82 @@ class MasterBtn {
     }
 
     init() {
-        this.setupMasterBtn();
-        this.setupMasterFloatingContent();
+        
     }
 
-    setupMasterBtn() {
-        const { masterBtnTitle } = this.options;
-        const { masterBtnClass } = this.parent;
+    setupFloatingContent() {
+    }
 
-        const masterBtn = document.createElement('div');
-        masterBtn.classList.add(masterBtnClass);
-        masterBtn.innerHTML = `<span title="${masterBtnTitle}">
+    toggleFloatingContentVisibility() {
+    }
+
+    addMenuEntry(filterItem) {
+    }
+}
+
+class PickerBtn {
+    constructor(parent) {
+        this.parent = parent;
+        this.options = parent.options;
+
+        this.init();
+    }
+
+    init() {
+        this.setupPickerBtn();
+        this.setupFloatingContent();
+    }
+
+    setupPickerBtn() {
+        const { pickerBtnTitle } = this.options;
+
+        const pickerBtn = document.createElement('div');
+        pickerBtn.classList.add(ClassNames.PICKER_BTN);
+        pickerBtn.innerHTML = `<span title="${pickerBtnTitle}">
             <i class="fas fa-filter"></i>
         </span>`;
         
-        masterBtn.addEventListener('click', this.toggleMasterFilterFloatingContentVisibility.bind(this));
+        pickerBtn.addEventListener('click', this.toggleFloatingContentVisibility.bind(this));
 
-        this.parent.container.appendChild(masterBtn);
+        this.parent.container.appendChild(pickerBtn);
     }
 
     disableMenuEntry(id) {
         const menuEntry = document.getElementById(id + '-menu-entry');
         if ( !menuEntry ) return;
-        menuEntry.classList.add('is-readonly');
+        menuEntry.classList.add(ClassNames.IS_READONLY);
     }
 
     enableMenuEntry(id) {
         const menuEntry = document.getElementById(id + '-menu-entry');
         if ( !menuEntry ) return;
-        menuEntry.classList.remove('is-readonly');
+        menuEntry.classList.remove(ClassNames.IS_READONLY);
     }
 
-    setupMasterFloatingContent() {
-        // attach floating dropdown to filterMasterBtn
+    setupFloatingContent() {
+        // attach floating dropdown to filterPickerBtn
         const attachFn = this.options.attachDropdown;
-        const { floatingContentClass, masterBtnFloatingContentClass, masterBtnClass } = this.parent;
 
         if (typeof attachFn === 'function') {
             // create a new div on document root
             const newNode = document.createElement('div');
-            newNode.classList.add(floatingContentClass);
-            newNode.classList.add(masterBtnFloatingContentClass);
+            newNode.classList.add(ClassNames.FLOATING_CONTENT);
+            newNode.classList.add(ClassNames.PICKER_FLOATING_CONTENT);
 
             // add html
-            newNode.innerHTML += `<div class="master-filter-content">
-                <div class="filter-content-header">
+            newNode.innerHTML += `<div class="${ClassNames.PICKER_FLOATING_CONTENT}">
+                <div class="${ClassNames.SELECTOR_CONTENT_HEADER}">
                     <strong>Filtros</strong>
                 </div>
-                <div class="filter-content-body">
+                <div class="${ClassNames.SELECTOR_CONTENT_BODY}">
                 </div>
             </div>`;
 
             // add to dom
             this.parent.container.appendChild(newNode);
 
-            // get source element ( master-filter-btn )
-            const source = document.querySelector('.' + masterBtnClass);
+            // get source element ( picker-filter-btn )
+            const source = document.querySelector('.' + ClassNames.PICKER_BTN);
 
             // call the attach function to attach the dropdown to the new node, so we can be framework agnostic
             attachFn(source, newNode);
@@ -130,11 +179,9 @@ class MasterBtn {
     }
 
     addMenuEntry(filterItem) {
-        const { masterBtnFloatingContentClass } = this.parent;
-
         // add to dropdown .filter-bar-main-dropdown-content
-        const ddContent = document.querySelector('.' + masterBtnFloatingContentClass);
-        const listBody = ddContent.querySelector('.filter-content-body');
+        const ddContent = document.querySelector('.' + ClassNames.PICKER_FLOATING_CONTENT);
+        const listBody = ddContent.querySelector('.' + ClassNames.SELECTOR_CONTENT_BODY);
 
         if ( this.options.immediateDisplay ) {
             this.addToScreen();
@@ -143,24 +190,25 @@ class MasterBtn {
         listBody.appendChild(filterItem);
     }
 
-    toggleMasterFilterFloatingContentVisibility() {
-        const { masterBtnFloatingContentClass, masterBtnClass } = this.parent;
-
-        const ddContent = document.querySelector('.' + masterBtnFloatingContentClass);
+    toggleFloatingContentVisibility() {
+        const ddContent = document.querySelector('.' + ClassNames.PICKER_FLOATING_CONTENT);
         ddContent.style.display = ddContent.style.display == 'block' ? 'none' : 'block';
 
         if ( this.options.onToggleDropdown ) {
-            const source = document.querySelector('.' + masterBtnClass);
+            const source = document.querySelector('.' + ClassNames.PICKER_BTN);
             this.options.onToggleDropdown(source, ddContent);
         }
     }
 }
 
-class FilterBarFilter {
-    constructor(opts) {
-        this.defaultOptions = {
+class HpvFilterContext {
+    constructor(opts = {}) {
+        this.options = {
             immediateDisplay: false,
             uniqueInstance: true,
+            disabledSelector: false,
+            picker: {},
+            selector: {},
             // fire call back when filter is added to filterBar
             onAddToBar: (filterbar, filter) => {},
             // fire call back when filter is removed from filterBar and user no longer can interact with it
@@ -173,6 +221,7 @@ class FilterBarFilter {
             onCreateDom: (filterbar, filter) => {},
             // allow custom rules extraction implementation
             onGetRules: (filterbar, filter) => {},
+            ...opts
         }
 
         // generate random id if not provided
@@ -180,7 +229,6 @@ class FilterBarFilter {
             opts.id = 'filter-' + Math.random().toString(36).substring(7);
         }
 
-        this.options = Object.assign({}, this.defaultOptions, opts);
         this.parent = null;
         // count how many instances of this filter are in the filterBar
         this.instances = 0;
@@ -192,15 +240,19 @@ class FilterBarFilter {
     }
 
     getLabel() {
-        return this.options.label;
+        return this.options.picker.label;
     }
 
     getFaIcon() {
-        return this.options.faIcon;
+        return this.options.picker.faIcon;
     }
 
     // reference to filterBar
-    setParent(parent) {
+    setBar(parent) {
+        if ( ! parent instanceof HpvFilterBar ) {
+            throw new Error('Parent must be an instance of HpvFilterBar');
+        }
+
         this.parent = parent;
     }
 
@@ -208,15 +260,19 @@ class FilterBarFilter {
         const filterItem = document.createElement('a');
         filterItem.id = this.options.id + '-menu-entry';
         filterItem.href = '#';
-        filterItem.classList.add('filter-content-item');
+        filterItem.classList.add(ClassNames.SELECTOR_CONTENT_ITEM);
         filterItem.innerHTML = `<i class="${this.getFaIcon()}"></i> <span class="menu-text">${this.getLabel()}</span>`;
 
         filterItem.addEventListener('click', (e) => {
             e.preventDefault();
-            // add to filter bar
-            this.addToScreen();
-            // hide dropdown
-            this.parent.masterBtn.toggleMasterFilterFloatingContentVisibility();
+
+            filterItem.dispatchEvent(new CustomEvent('pickerBtnItemSelection', {
+                detail: {
+                    filter: this
+                },
+                bubbles: true,
+                cancelable: true
+            }));
         });
 
         return filterItem;
@@ -235,28 +291,48 @@ class FilterBarFilter {
         console.log(`Adding filter ${this.options.id} to screen`);
 
         const filterSelectorBtn = document.createElement('div');
-        filterSelectorBtn.classList.add('selector-btn');
+        filterSelectorBtn.classList.add(ClassNames.SELECTOR_BTN);
         filterSelectorBtn.id = this.options.id + '-selector-btn';
 
         const filterSelectorText = document.createElement('span');
-        filterSelectorText.classList.add('selector-btn-text');
-        filterSelectorText.innerText = this.options.label;
+        filterSelectorText.classList.add(ClassNames.SELECTOR_BTN_TEXT);
+        filterSelectorText.innerText = this.options.picker.label;
 
         const filterSelectorArrowContainer = document.createElement('div');
-        filterSelectorArrowContainer.classList.add('selector-btn-arrow-container');
+        filterSelectorArrowContainer.classList.add(ClassNames.SELECTOR_BTN_ARROW_CONTAINER);
 
         const filterSelectorArrow = document.createElement('span');
-        filterSelectorArrow.classList.add('selector-btn-arrow');
+        filterSelectorArrow.classList.add(ClassNames.SELECTOR_BTN_ARROW);
 
-        filterSelectorArrowContainer.appendChild(filterSelectorArrow);
         filterSelectorBtn.appendChild(filterSelectorText);
-        filterSelectorBtn.appendChild(filterSelectorArrowContainer);
 
-        // get position of master-btn
-        const masterBtn = document.querySelector('.' + this.parent.masterBtnClass);
+        if ( !this.options.disabledSelector ) {
+            filterSelectorArrowContainer.appendChild(filterSelectorArrow);
+            filterSelectorBtn.appendChild(filterSelectorArrowContainer);
+        } else {
+            filterSelectorBtn.classList.add(ClassNames.SELECTOR_BTN_DISABLED);
+        }
 
-        // add filterSelectorBtn before masterBtn
-        masterBtn.parentNode.insertBefore(filterSelectorBtn, masterBtn);
+        // get position of picker-btn
+        const pickerBtn = document.querySelector('.' + ClassNames.PICKER_BTN);
+
+        // add filterSelectorBtn before pickerBtn
+        pickerBtn.parentNode.insertBefore(filterSelectorBtn, pickerBtn);
+
+        if ( !this.options.disabledSelector ) {
+            // add click listener to filterSelectorBtn
+            filterSelectorBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                filterSelectorBtn.dispatchEvent(new CustomEvent('filterBtnItemSelection', {
+                    detail: {
+                        filter: this
+                    },
+                    bubbles: true,
+                    cancelable: true
+                }));
+            });
+        }
 
         this.options.onAddToBar(this.parent, this);
 
@@ -267,10 +343,10 @@ class FilterBarFilter {
         if ( this.options.uniqueInstance ) {
             if ( this.instances == 1 ) {
                 this.allowMoreInstances = false;
-                this.parent.masterBtn.disableMenuEntry(this.options.id);
+                this.parent.pickerBtn.disableMenuEntry(this.options.id);
             } else {
                 this.allowMoreInstances = true;
-                this.parent.masterBtn.enableMenuEntry(this.options.id);
+                this.parent.pickerBtn.enableMenuEntry(this.options.id);
             }
         }
     }
@@ -284,7 +360,9 @@ class FilterBarFilter {
 
         this.instances--;
 
-        this.options.onRemoveFromBar(this.parent, this);
+        if ( this.options.onRemoveFromBar ) {
+            this.options.onRemoveFromBar(this.parent, this);
+        }
 
         this.processMaxInstances();
     }
